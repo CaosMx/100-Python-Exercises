08982a79972563197f16ff863142f486619a3136
Manuel Jesus Arreola Sandoval <ing.manuel.arreola@gmail.com>
2020-11-18 21:14:28 -0600

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,19 @@
+"""
+
+Exercise 1 - Variable Updating
+
+Question: Let's start with easy things first. What will the following code produce?
+
+    a = 2
+    a = 4
+    a = 6
+    print(a + a + a)
+
+"""
+
+a = 2
+a = 4
+a = 6
+print (a+a+a)
+
+# (6+6+6)=18 -> 1 Punto
