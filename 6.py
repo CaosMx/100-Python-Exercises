bcf51249109b8ffce86e75087354acde11303d41
Manuel Jesus Arreola Sandoval <ing.manuel.arreola@gmail.com>
2020-11-18 21:19:04 -0600

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,18 @@
+"""
+
+Exercise 6 - Sequence Slicing
+
+Question: Please complete the script so that it prints out a list slice containing items d , e , and f .
+
+    letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+Expected output: 
+
+['d', 'e', 'f']
+
+"""
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+print (letters[3:6])
+
+#['d', 'e', 'f'] -> 1 Punto
