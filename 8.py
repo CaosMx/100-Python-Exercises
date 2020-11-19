96b6496e30c8e41cf153b470003242fd9859dc2d
Manuel Jesus Arreola Sandoval <ing.manuel.arreola@gmail.com>
2020-11-18 21:19:48 -0600

    Create 8.py

---
+++ 8.py
@@ -0,0 +1,27 @@
+"""
+
+Exercise 8 - Negative Indexing
+
+Question: Complete the script so that it prints out the letter i  using negative indexing.
+
+    letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+Expected output: 
+
+i 
+
+"""
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+print (letters[-2])
+
+# i -> 1 Punto
+
+"""
+
+H   O   L   A   -> Palabra
+1   2   3   4   -> Index +
+-4  -3  -2  -1  -> Index -
+
+"""
