d5ac33bd3839253db92ec3658aeb408504596e45
Manuel Jesus Arreola Sandoval <ing.manuel.arreola@gmail.com>
2020-11-18 21:20:09 -0600

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,19 @@
+"""
+Exercise 9 - Negative Slicing
+
+Question: Complete the script so that it prints out a list slice containing the last three items of the list letters .
+
+    letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+Expected output:
+
+['h', 'i', 'j']
+
+
+"""
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+print (letters[-3:])
+
+# ['h', 'i', 'j'] -> 1 Punto
