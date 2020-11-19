26b203f47b69a66396c5a34c502af5404a045967
Manuel Jesus Arreola Sandoval <ing.manuel.arreola@gmail.com>
2020-11-18 21:18:35 -0600

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,18 @@
+"""
+
+Exercise 5 - Sequence Indexing
+
+Question: Complete the script so that it prints out the second item of the list.
+
+    letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+Expected output: 
+
+b
+
+"""
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+print (letters[1])
+
+#b -> 1 Punto
