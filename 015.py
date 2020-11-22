1b06b8b90b2e19b1ed7d350f39cbd7f66824c6dd
Manuel Jesus Arreola Sandoval <ing.manuel.arreola@gmail.com>
2020-11-21 19:03:02 -0600

    Create 015.py

---
+++ 015.py
@@ -0,0 +1,16 @@
+"""
+Exercise 15 - Simple Dictionary
+
+Question: Create a dictionary that contains the keys a  and b  and their respective values 1  and 2 .
+
+
+"""
+diccionario = dict(a=1, b=2)
+print (diccionario)
+
+# Otra forma es: Using curly brackets is one way to create a dictionary.
+
+d = {"a":1, "b":2}
+print (d)
+
+# {'a': 1, 'b': 2} -> 1 Punto
