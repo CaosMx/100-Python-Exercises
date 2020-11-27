a9bcb100baf8455c4c1f8822be4b91af0a1baa21
Manuel Jesus Arreola Sandoval <ing.manuel.arreola@gmail.com>
2020-11-26 20:27:54 -0600

    Create 016.py

---
+++ 016.py
@@ -0,0 +1,22 @@
+"""
+Exercise 16 - Accessing Dictionary Items
+
+Question: Please complete the script so that it prints out the value of key b .
+
+    d = {"a": 1, "b": 2}
+
+Expected output: 
+
+2  
+
+"""
+
+d = {"a": 1, "b": 2}
+print (d["b"])
+
+# 2 -> 1 punto
+
+"""
+As you see, accessing dictionary values follows the same syntax as accessing list items. 
+The difference is that lists have indexes, while dictionaries have keys that you create by yourself.
+"""
